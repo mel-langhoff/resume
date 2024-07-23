297f89d9b22a607073649dfab484e53456ba3da3
Mel <mel@Mels-MacBook-Pro.local>
2024-07-23 12:58:18 -0600

    Test push

---
+++ spec/features/home/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Root Page', type: :feature do
+  describe 'when a user visits the root path /' do
+    it "has links to about, resume, portfolio, contact" do
+
+    end
+
+    it "has a link to vinyl collection" do
+
+    end
+  end
+end
