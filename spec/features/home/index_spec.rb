6287df155692efc14503497a6578c7e28aa8f81f
Mel <mel@Mels-MacBook-Pro.local>
2024-07-23 13:03:51 -0600

    Add test for header

--- spec/features/home/index_spec.rb
+++ spec/features/home/index_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Root Page', type: :feature do
   describe 'when a user visits the root path /' do
-    it "has links to about, resume, portfolio, contact" do
+    describe 'header' do
+      it "has links to about, resume, projects, contact, & vinyl collection" do
+        visit root_path
 
-    end
-
-    it "has a link to vinyl collection" do
-
+        expect(page).to have_link("About")
+        expect(page).to have_link("Resume")
+        expect(page).to have_link("Projects")
+        expect(page).to have_link("Contact")
+        expect(page).to have_link("Vinyl Collection")
+      end
     end
   end
 end
