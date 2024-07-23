92ede9e51b68755db0e19798335fc7ccf5be0c66
Mel <mel@Mels-MacBook-Pro.local>
2024-07-23 16:31:09 -0600

    Add projects to seeds.rb

--- spec/features/projects/index_spec.rb
+++ spec/features/projects/index_spec.rb
@@ -6,21 +6,6 @@ RSpec.describe 'Resume Index Page', type: :feature do
   end
 
   it 'contains a jobs attributes' do
-    visit resume_path
-
-    within '#job-description-1' do
-      expect(page).to have_content(@xcel.title)
-      expect(page).to have_content(@xcel.company)
-      expect(page).to have_content(@xcel.location)
-      expect(page).to have_content(@xcel.formatted_start_date)
-      expect(page).to have_content(@xcel.formatted_end_date)
-      expect(page).to have_content(@xcel.description)
-    end
-  end
-  
-  it 'lists jobs in descending order by start date' do
-    visit resume_path
-
-    expect("Xcel").to appear_before("Crocs")
+    
   end
 end
