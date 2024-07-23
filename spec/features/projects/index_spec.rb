d940d7e96dca461b2f46dc145d2e0931130b1a00
Mel <mel@Mels-MacBook-Pro.local>
2024-07-23 16:35:32 -0600

    Projects index complete

--- spec/features/projects/index_spec.rb
+++ spec/features/projects/index_spec.rb
@@ -2,10 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Resume Index Page', type: :feature do
   before :each do
-    
+    @market_money = Project.create!(
+      id: 1,
+      title: "Market Money",
+      description: "Developing an API enabling access to local farmers markets and vendors.",
+      git_repo: "https://github.com/JaredMHobson/market_money",
+      tech: "Ruby on Rails"
+    )
+
+    @battleship = Project.create!(
+      id: 2,
+      title: "Battleship",
+      description: "Terminal Ruby game of Battleship developed with a partner.",
+      git_repo: "https://github.com/mel-langhoff/battleship",
+      tech: "Ruby"
+    )
   end
 
-  it 'contains a jobs attributes' do
-    
+  it 'contains a projects attributes' do
+    visit projects_path
+
+    within '#project-description-2' do
+      expect(page).to have_content(@battleship.title)
+      expect(page).to have_link('Github Repository', href: @battleship.git_repo)
+      expect(page).to have_content(@battleship.tech)
+      expect(page).to have_content(@battleship.description)
+    end
   end
 end
