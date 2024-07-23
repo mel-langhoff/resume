9601ad792ee5c582304ff3dd02b4f25ec6baed41
Mel <mel@Mels-MacBook-Pro.local>
2024-07-23 16:44:25 -0600

    Add spec test, passes

---
+++ spec/features/contact/index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Contact Page', type: :feature do
+  it 'has a link to email' do
+    visit contact_path
+
+    expect(page).to have_link('hello@mel-langhoff.com', href: 'mailto:hello@mel-langhoff.com')
+  end
+end
