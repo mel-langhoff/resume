9f442008baf3bdd33ef16de42a00026c550dc42f
Mel <mel@Mels-MacBook-Pro.local>
2024-08-11 13:08:02 -0600

    Adjust content box style

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ApplicationRecord
   validates_presence_of :title, :company, :start_date, :description
 
   def self.sort_jobs
-    order(start_date: :desc).distinct
+    order(id: :asc).distinct
   end
 
   def formatted_start_date
