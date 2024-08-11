93a19136d92ff15f3d700e4a5c0c339cd1d66427
Mel <mel@Mels-MacBook-Pro.local>
2024-08-11 15:13:32 -0600

    Model test complete

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ApplicationRecord
-  validates_presence_of :title, :git_repo, :tech, :description, :type
+  validates_presence_of :title, :git_repo, :tech, :description, :project_type
+
+  def self.sort_by_type(type)
+    if type == 'All'
+      all
+    else
+      where(project_type: type).order(:title)
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Project, type: :model do
     description: "Developing an API enabling access to local farmers markets and vendors.",
     git_repo: "https://github.com/JaredMHobson/market_money",
     tech: "Ruby on Rails",
-    type: "rails"
+    project_type: "rails"
   )
 
   @battleship = Project.create!(
@@ -15,7 +15,7 @@ RSpec.describe Project, type: :model do
     description: "Terminal Ruby game of Battleship developed with a partner.",
     git_repo: "https://github.com/mel-langhoff/battleship",
     tech: "Ruby",
-    type: "ruby"
+    project_type: "ruby"
   )
 
   @portfolio = Project.create!(
@@ -24,7 +24,7 @@ RSpec.describe Project, type: :model do
     git_repo: "https://github.com/mel-langhoff/resume",
     demo_link: "www.mel-langhoff.com",
     tech: "Rails",
-    type: "rails"
+    project_type: "rails"
   )
   end
 
@@ -33,7 +33,7 @@ RSpec.describe Project, type: :model do
     it { should validate_presence_of :git_repo }
     it { should validate_presence_of :tech }
     it { should validate_presence_of :description }
-    it { should validate_presence_of :type }
+    it { should validate_presence_of :project_type }
   end
 
 
@@ -45,7 +45,16 @@ RSpec.describe Project, type: :model do
 
   end
 
-  it '#sort_by_type' do
-  
+  describe '#sort_by_type' do
+    it 'sorts by a specificied type' do
+      rails_projects = Project.sort_by_type('rails')
+      expect(rails_projects).to eq([@market_money, @portfolio])
+      expect(rails_projects).to_not include(@battleship)
+    end
+
+    it 'returns all projects for a non specified type' do
+      all_projects = Project.sort_by_type('All')
+      expect(all_projects).to include(@battleship, @market_money, @portfolio)
+    end
   end
 end
