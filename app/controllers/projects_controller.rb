a60391d6f96db23bb41ed2ad9abf05726e79cab8
Mel <mel@Mels-MacBook-Pro.local>
2024-08-24 13:22:39 -0600

    Refine proj sort option

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,10 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all.distinct
+    @projects = Project.all
     @types = Project.types
     type = params[:project_type] || 'All'
-    @alphabetically = @projects.alphabetical
-    @by_type = @projects.sort_by_type(type)
+    # @alphabetically = @projects.alphabetical
+    @by_type = @projects.alphabetical.sort_by_type(type)
     # @by_project_link = @projects.project_link?
   end
 
